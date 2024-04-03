4c95fa34c01b1603e8c6242928a6150b49dda275
Florian <florian@vetter-de.net>
2024-04-03 19:55:56 +0200

    Initial Commit

---
+++ SpotifyUIClone/SpotifyUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUICloneApp.swift
+//  SpotifyUIClone
+//
+//  Created by Florian on 03.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
